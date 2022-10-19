22c2d78b2667e418832cc734bec0c381ff059c1e
sanjib0827 <sahoosanjib521@gmail.com>
2022-10-19 13:47:06 +0530

    arithmatic operations of pycharm

--- pythonproject_1/project_tutorial/project_class_8.py
+++ pythonproject_1/project_tutorial/project_class_8.py
@@ -21,7 +21,13 @@ output = "sub of " + str(x) + " and " + str(y) + " is " + str(sub)
 print(output)
 
 # [TODO] - MULTIPLICATION
+# multiplication
+print("multiplication", x * y)
+mult = x * y
+print("multiplication of", x, "and", y, "is", mult)
 
+output = "multiplication of " + str(x) + " and " + str(y) + " is " + str(mult)
+print(output)
 # divsion
 print("division:", x / y)
 print("division:", x // y)
@@ -34,6 +40,11 @@ print(output)
 # modulus(also known as remainder, also known as mod)
 print("mod:", x % y)
 #[TODO] - modulus complete
+mod = x % y
+print("mod of", x, "and", y, "is", mod)
+
+output = "mod of " + str(x) + " and " + str(y) + " is " + str(mod)
+print(output)
 
 # square of a number
 print("square of", x, "is", x * x)
@@ -45,13 +56,16 @@ output = "square of " + str(y) + " is " + str(y * y)
 print(output)
 
 # [TODO] - cube
-
-
+print("cube", x ** 3)
+print("cube of", y ** 3)
 # x to the power y
 print("x to the power y", x ** y)
 
 # [TODO] - COMPLETE X TO THE POWER Y
-
+xpowery = "power of ", x, "and", y, "is", x ** y
+print(xpowery)
+output = "power of " + str(x) + " and " + str(y) + " is " + str(x **y)
+print(output)
 # SQUARE ROOT
 
 import math
