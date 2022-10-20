380743c521568b4d9d4eec7cf9597caba27c26ea
sanjib0827 <sahoosanjib521@gmail.com>
2022-10-20 15:13:21 +0530

    array and for loop

---
+++ pythonproject_1/project_tutorial/project_class_9.py
@@ -0,0 +1,17 @@
+# x = int(input("enter first number"))
+# y = int(input("enter second number"))
+# print(type(x))
+# print(x + y)
+
+z = input("enter name: ")
+print(z)
+print(len(z))
+print(z.count("a"))
+# print the second character of the given word stored in variable z
+print(z[2])
+print(z[8])
+index_to_display = int(input("enter the index to visualize the character stored in z: "))
+print(z[index_to_display])
+# slicing
+print(z[7:12])
+print(z[0:7])
---
+++ pythonproject_1/project_tutorial/project_class_9_part2.py
@@ -0,0 +1,18 @@
+# for loop
+k = "sanjib sahoo"
+
+# for character in k:
+#     print("the character is:", character)
+#     print("happy diwali")
+# print("happy kali puja")
+
+
+for char in k:
+    print("the character is:", char)
+    if "i" == char:
+        print("happy diwali")
+    elif "s" == char:
+        print("my name is sanjib")
+print("happy holidays")
+print("go to home")
+print("good food")
